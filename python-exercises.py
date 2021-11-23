fe8de89214e7f094b8c3d42cc517633874306706
Akash <83765015+akashrajan5@users.noreply.github.com>
2021-11-23 12:36:39 +0530

    Update python-exercises.py

--- python-exercises.py
+++ python-exercises.py
@@ -1,1 +1,20 @@
+# Finding the highest of three numbers using command line input.
 
+import sys
+
+try:
+    a = len(sys.argv)
+    b = int(sys.argv[1])
+    c = int(sys.argv[2])
+    d = int(sys.argv[3])
+    if(a < 4 or a > 4):
+        print('Please provide three numbers')
+    else:
+        if(b > c and b > d):
+            print(f'{b} is the highest')
+        elif(c > d and c > b):
+            print(f'{c} is the highest')
+        else:
+            print(f'{d} is the highest')
+except ValueError:
+    print('Please provide integers')
