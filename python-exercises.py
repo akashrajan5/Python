70fb185386af0fc39d0a729de20020ab44411517
Akash <akashrajan5@gmail.com>
2021-11-27 18:42:34 +0530

    Update python-exercises.py

--- python-exercises.py
+++ python-exercises.py
@@ -29,6 +29,20 @@ def revUsingLoop(e):
         s = i + s
     print(s)
 
+#FizzBuzz program
+def fizz():
+    for i in range(100):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+            continue
+        elif i % 3 == 0:
+            print('Fizz')
+            continue
+        elif i % 5 == 0:
+            print('Buzz')
+            continue
+        print(i)
+
 #Scraping
 import requests
 from bs4 import BeautifulSoup
